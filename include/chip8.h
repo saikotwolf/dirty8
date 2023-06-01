517b714396ebc66cb2a2490f789d9ce873d9caf1
Gabriel Romero <gromero.eg@gmail.com>
2023-06-01 16:18:47 -0400

    Add memory to chip8

--- include/chip8.h
+++ include/chip8.h
@@ -3,12 +3,14 @@
 #define CHIP8_H
 
 #include "registers.h"
+#include "memory.h"
 #include "stack.h"
 
 struct chip8_context
 {
     chip8_registers registers;
     chip8_stack stack;
+    chip8_memory memory;
 };
 
 #endif
