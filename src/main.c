ec6ec02679b296e6fe7092a1a4448c8c061f0a81
Gabriel Romero <gromero.eg@gmail.com>
2023-06-01 15:57:14 -0400

    Change direct value by macro

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv)
 
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow(
-        "dirty8 emulator",
+        CHIP8_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER,
