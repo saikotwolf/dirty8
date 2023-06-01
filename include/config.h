72ba7ea56bc5257c9c321cf6f78c0de07f74a4be
Gabriel Romero <gromero.eg@gmail.com>
2023-06-01 15:56:15 -0400

    Update config.h

--- include/config.h
+++ include/config.h
@@ -2,8 +2,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define CHIP8_WINDOW_TITLE "dirty8 emulator"
 #define CHIP8_WIDTH 64
 #define CHIP8_HEIGHT 32
 #define CHIP8_WINDOW_MULTIPLIER 10
+
+#define CHIP8_STACK_DEPTH_SIZE 16
 
 #endif
