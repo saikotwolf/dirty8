a093e40b686d2e126662b3b652a7702bc0d8b932
Gabriel Romero <gromero.eg@gmail.com>
2023-06-17 13:31:53 -0400

    Add chip8_screen to the struct chip8_context and chip8_init header

--- include/chip8.h
+++ include/chip8.h
@@ -4,6 +4,7 @@
 
 #include "registers.h"
 #include "memory.h"
+#include "screen.h"
 #include "stack.h"
 
 struct chip8_context
@@ -11,6 +12,9 @@ struct chip8_context
     chip8_registers registers;
     chip8_stack stack;
     chip8_memory memory;
+    chip8_screen screen;
 };
+
+void chip8_init(struct chip8_context *chip8);
 
 #endif
